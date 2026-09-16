59f3f2c959933603154b7915de926afa2523bbdb
Mykola Stepanov <ukrnikolas@gmail.com>
2026-09-17 00:16:17 +0300

    Add Grafana Loki task2 solution 3

--- main.go
+++ main.go
@@ -1,30 +1,12 @@
 package main
 
 import (
-    "github.com/labstack/echo/v4"
-	"webapp/pkg/config"
-)
-
-func main() {
-	port := config.GetEnv("PORT", "3000")
-
-    e := echo.New()
-    e.Static("/", "public")
-    
-    e.GET("/", func(c echo.Context) error {
-        return c.File("public/views/webapp.html")
-    })
-    e.Start(":" + port)
-}
-
-package main
-
-import (
 	"os"
+
+	"webapp/pkg/config"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"webapp/pkg/config"
 )
 
 func main() {
