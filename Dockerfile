878d83f75a596c612bbf66ca40ef1a7ce85b1d4e
Mykola Stepanov <ukrnikolas@gmail.com>
2026-09-17 00:08:01 +0300

    Add Grafana Loki task2 solution

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,13 @@
+FROM golang:1.20-alpine AS builder
+WORKDIR /app
+COPY go.mod go.sum ./
+RUN go mod download
+COPY . .
+RUN go build -o webapp .
 
+FROM alpine:latest
+WORKDIR /app
+COPY --from=builder /app/webapp .
+COPY --from=builder /app/public ./public
+EXPOSE 3000
+CMD ["./webapp"]
--- main.go
+++ main.go
@@ -16,3 +16,34 @@ func main() {
     })
     e.Start(":" + port)
 }
+
+package main
+
+import (
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"webapp/pkg/config"
+)
+
+func main() {
+	port := config.GetEnv("PORT", "3000")
+	logPath := config.GetEnv("LOG_PATH", "/var/log/webapp/app.log")
+
+	logFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	e := echo.New()
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Output: logFile,
+	}))
+	e.Static("/", "public")
+
+	e.GET("/", func(c echo.Context) error {
+		return c.File("public/views/webapp.html")
+	})
+	e.Start(":" + port)
+}
